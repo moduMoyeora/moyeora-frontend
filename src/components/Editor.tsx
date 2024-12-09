c4994308443d451cf40c81489ce57ca9c01bc5bd
ShJang32 <jangseohie32@gamil.com>
2024-12-09 18:36:36 +0900

    Toast UI 세팅 수정

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,22 +1,47 @@
-import { Editor } from '@toast-ui/react-editor'
 import '@toast-ui/editor/dist/toastui-editor.css'
+import { Editor } from '@toast-ui/react-editor'
+import React, { useRef, useState } from 'react'
 
-const EditorCommon = () => {
+const TuiEditor: React.FC = () => {
+  const editorRef = useRef<Editor | null>(null) // 명시적으로 Editor 타입을 설정
+  const [content, setContent] = useState<string>('') // 상태를 관리
+
+  const handleBold = () => {
+    if (editorRef.current) {
+      editorRef.current.getInstance().exec('bold')
+    }
+  }
+
+  const handleSave = () => {
+    if (editorRef.current) {
+      const editorContent = editorRef.current.getInstance().getMarkdown()
+      setContent(editorContent) // 에디터 내용을 상태에 저장
+    }
+  }
+
   return (
-    <Editor
-      previewStyle="vertical"
-      initialEditType="wysiwyg"
-      placeholder="글을 작성해 주세요"
-      height="450px"
-      toolbarItems={[
-        ['bold', 'italic', 'strike'],
-        ['hr'],
-        ['image', 'link'],
-        ['ul', 'ol'],
-        ['code', 'codeblock'],
-      ]}
-    />
+    <>
+      <Editor
+        previewStyle="vertical"
+        height="400px"
+        initialEditType="markdown"
+        initialValue="hello"
+        ref={editorRef}
+      />
+      <div id="toastUIEditor">
+        <h1>Toast UI Editor Example</h1>
+        <div id="button">
+          <button className="btn_save" onClick={handleSave}>
+            Save
+          </button>
+        </div>
+        <div>
+          <h2>Result</h2>
+          <textarea className="result" value={content} readOnly></textarea>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default EditorCommon
+export default TuiEditor
