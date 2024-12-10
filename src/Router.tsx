0cae8740b6678c93124273caae236f4f7a2c6bb5
ShJang32 <jangseohie32@gamil.com>
2024-12-10 12:37:35 +0900

    Router에 사용하지 않는 파일 import 삭제

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,7 +3,6 @@ import App from './App'
 import Home from './pages/home'
 import TuiEditor from './components/Editor'
 import React from 'react'
-import Post from './pages/Post'
 
 const Router: React.FC = () => {
   return (
