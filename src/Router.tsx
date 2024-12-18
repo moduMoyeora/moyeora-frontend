f1a696b27f92623c93ec3a94fd32faae09364819
Yubeenpark <yoobinpark@gmail.com>
2024-12-18 22:38:45 +0900

    #    modified:   src/Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import App from './App'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import MyPage from './pages/MyPage'
