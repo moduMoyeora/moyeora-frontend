4cf09554333d54d1547af5cd8cd05ad14242ad87
Yubeenpark <yoobinpark@gmail.com>
2024-12-18 22:23:51 +0900

    feat: add TypeScript types for React Router and update dependencies
    - Added @types/react-router-dom and @types/react-router to package.json and package-lock.json for improved TypeScript support.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import './App.css'
 
-import { BrowserRouter } from 'react-router-dom'
-import { Button } from '@mui/material'
 import NavBar from './components/Navbar'
-import React from 'react'
 import Router from './Router'
-import Signup from './pages/Signup'
-import logo from './logo.svg'
 
 function App() {
   return (
