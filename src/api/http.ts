e4c7a9182f1b53d8079593742f6e8fe5fe629fb7
Yubeenpark <yoobinpark@gmail.com>
2024-12-20 23:03:38 +0900

    refactor: remove type of headers from axios instance creation

--- src/api/http.ts
+++ src/api/http.ts
@@ -8,10 +8,6 @@ export const createClient = (config?: AxiosRequestConfig) => {
   const axiosInstance = axios.create({
     baseURL: BASE_URL,
     timeout: DEFAULT_TIMEOUT,
-    headers: {
-      'Content-Type': 'application/json',
-      // ...(getToken() ? { Authorization: `Bearer ${getToken()}` } : {}),
-    },
     withCredentials: true,
     ...config,
   })
