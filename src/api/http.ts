919b9fc06666b4196773e6be08811ee39bfdafbe
Yubeenpark <yoobinpark@gmail.com>
2024-12-20 23:02:26 +0900

    error(CORS):  delete 'Content-Type': 'application/json',s from Axios client configuration

--- src/api/http.ts
+++ src/api/http.ts
@@ -8,10 +8,6 @@ export const createClient = (config?: AxiosRequestConfig) => {
   const axiosInstance = axios.create({
     baseURL: BASE_URL,
     timeout: DEFAULT_TIMEOUT,
-    headers: {
-      'Content-Type': 'application/json',
-      // ...(getToken() ? { Authorization: `Bearer ${getToken()}` } : {}),
-    },
     withCredentials: true,
     ...config,
   })
@@ -39,7 +35,5 @@ export const createClient = (config?: AxiosRequestConfig) => {
     }
   )
   return axiosInstance
-
 }
 export const httpClient = createClient()
-
